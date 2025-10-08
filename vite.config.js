bbc71badaea5eb7bde60277c344732f330b9a4b8
jair david <cjair759@gmail.com>
2025-10-08 00:46:51 -0500

    se crea el archvo vite.config 6

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
-export default {
-  server: {
-    allowedHosts: ['.ngrok-free.app']
-  }
-}
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  vite: {
+    server: {
+      host: '0.0.0.0', // escucha todas las interfaces
+      port: 4321,
+      allowedHosts: ['*.ngrok-free.app'], // comodín para cualquier subdominio de ngrok
+    },
+  },
+});
